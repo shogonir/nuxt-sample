16f3d52d51f46cc864a75dea9b2adee154fef60e
Shogo N <shogonir@gmail.com>
2018-10-29 02:13:50 +0900

    [add] tab test implementation

---
+++ 01-ts-template-tabs/components/SnTab.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="tab-headers-area">
+      <div
+        v-for="(label, index) in labels"
+        :key="label"
+        :class="tabHeaderClass(index)"
+        @click="onClickTabHeader(index)"
+      >
+        {{ label }}
+      </div>
+    </div>
+    <div class="tab-contents-area">
+    </div>
+  </div>
+</template>
+
+
+
+<script lang="ts">
+
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class SnTab extends Vue {
+  
+  @Prop()
+  private commonClass: string
+
+  @Prop()
+  private activeClass: string
+
+  @Prop()
+  private inactiveClass: string
+
+  @Prop()
+  private labels: string[]
+
+  private activeTabIndex: number = 0
+
+  mounted() {
+    console.log(this.activeClass)
+  }
+
+  tabHeaderClass(index: number): string {
+    return index === this.activeTabIndex
+      ? `${this.commonClass} ${this.activeClass}`
+      : `${this.commonClass} ${this.inactiveClass}`
+  }
+
+  onClickTabHeader(index: number) {
+    this.activeTabIndex = index
+  }
+}
+
+</script>
+
+
+
+<style scoped>
+
+.tab-headers-area {
+  width: 100%;
+  height: 70px;
+  background-color: red;
+}
+
+.tab-contents-area {
+  width: 100%;
+  height: 100%;
+  background-color: blue;
+}
+
+.common-class {
+  width: 200px;
+  float: left;
+}
+
+.active-class {
+  background-color: green;
+}
+
+</style>
---
+++ 01-ts-template-tabs/pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <h1 class="header">Nuxt TypeScript Starter</h1>
+    <SnTab
+      commonClass="common-class"
+      activeClass="active-class"
+      inactiveClass="inactive-class"
+      :labels="['members', 'teams', 'categories']"
+    />
+  </section>
+</template>
+
+
+
+<script lang="ts">
+
+import { Component, Vue } from 'nuxt-property-decorator'
+import { State } from 'vuex-class'
+
+import SnTab from '~/components/SnTab.vue'
+
+@Component({
+  components: {
+    SnTab,
+  }
+})
+export default class extends Vue {
+}
+
+</script>
+
+
+
+<style scoped>
+.header {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana,
+    sans-serif;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
